2e21554a271fe76e04f873b36d4c51fce563e764
Mahmoud Omiesh <mahmoudomiesh2005@gmail.com>
2025-11-20 16:37:23 +0200

    feat: cors to allows localhost:port

--- app/server/api/index.ts
+++ app/server/api/index.ts
@@ -1,6 +1,7 @@
 import { createHonoServer } from "react-router-hono-server/node";
 import { Scalar } from "@scalar/hono-api-reference";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 export const app = new Hono();
 
@@ -12,6 +13,31 @@ import { mediaRoute } from "./routes/media";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const apiRoutes = app
   .basePath("/api")
+  .use(
+    cors({
+      origin: (origin) => {
+        //TODO: remove this in prod
+        if (
+          origin &&
+          (origin.startsWith("http://localhost:") ||
+            origin.startsWith("http://127.0.0.1:") ||
+            origin.startsWith("https://localhost:") ||
+            origin.startsWith("https://127.0.0.1:"))
+        ) {
+          return origin;
+        }
+
+        if (!origin) {
+          return "*";
+        }
+
+        return origin;
+      },
+      credentials: true,
+      allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+      allowHeaders: ["Content-Type", "Authorization"],
+    }),
+  )
   .route("/properties", propertiesRoute)
   .route("/me", usersRoute)
   .route("/media", mediaRoute);
